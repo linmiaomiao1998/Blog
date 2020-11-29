1786b08ef004966e9daac43879b77ff4ee3390d3
linmiaomiao1998 <994717247@qq.com>
2020-11-29 23:06:55 +0800

    用class选择内容

--- neko-ui-vue3/src/components/TabsDemo.vue
+++ neko-ui-vue3/src/components/TabsDemo.vue
@@ -1,6 +1,6 @@
 <template>
    <div>Tabs页面</div>
-   <Tabs>
+   <Tabs  v-model:selected="x">
    <Tab title="导航1">内容1</Tab>
    <Tab title="导航2">内容2</Tab>
    </Tabs>
@@ -9,10 +9,17 @@
 <script>
 import Tab from "../lib/Tab.vue"
 import Tabs from "../lib/Tabs.vue"
+import { ref } from 'vue';
 export default {
  components:{
     Tabs,Tab
- }
+ },
+  setup() {
+    const x = ref('导航1')
+    return {
+      x
+    }
+  }
 }
 </script>
 
