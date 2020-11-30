0ee2d97131b69077731cc924822d7964c93610c7
linmiaomiao1998 <994717247@qq.com>
2020-11-30 16:05:29 +0800

    下划线滑动，标签切换

--- neko-ui-vue3/src/lib/Tabs.vue
+++ neko-ui-vue3/src/lib/Tabs.vue
@@ -2,16 +2,22 @@
   <div>
     Tabs组件
     <div class="neko-tabs">
-      <div class="neko-tabs-nav">
+      <div class="neko-tabs-nav" ref="container">
         <div
           class="neko-tabs-nav-item"
           v-for="(t, index) in titles"
+          :ref="
+            (el) => {
+              if (el) navItems[index] = el;
+            }
+          "
           @click="select(t)"
           :class="{ selected: t === selected }"
           :key="index"
         >
           {{ t }}
         </div>
+        <div class="neko-tabs-nav-indicator" ref="indicator"></div>
       </div>
       <div class="neko-tabs-content">
         <component
@@ -27,7 +33,7 @@
 
 <script lang="ts">
   import Tab from "./Tab.vue";
-  import { computed } from "vue";
+  import { computed, ref, onMounted, onUpdated } from "vue";
   export default {
     props: {
       selected: {
@@ -35,6 +41,23 @@
       },
     },
     setup(props, context) {
+      const navItems = ref<HTMLDivElement[]>([]);
+      const indicator = ref<HTMLDivElement>(null);
+      const container = ref<HTMLDivElement>(null);
+      const x = () => {
+        const divs = navItems.value;
+        const result = divs.filter((div) =>
+          div.classList.contains("selected")
+        )[0];
+        const { width } = result.getBoundingClientRect();
+        indicator.value.style.width = width + "px";
+        const { left: left1 } = container.value.getBoundingClientRect();
+        const { left: left2 } = result.getBoundingClientRect();
+        const left = left2 - left1;
+        indicator.value.style.left = left + "px";
+      };
+      onMounted(x);
+      onUpdated(x)
       const defaults = context.slots.default();
       defaults.forEach((tag) => {
         if (tag.type !== Tab) {
@@ -57,6 +80,9 @@
         defaults,
         titles,
         select,
+        navItems,
+        indicator,
+        container
       };
     },
   };
@@ -71,6 +97,7 @@
       display: flex;
       color: $color;
       border-bottom: 1px solid $border-color;
+      position: relative;
       &-item {
         padding: 8px 0;
         margin: 0 16px;
@@ -81,6 +108,16 @@
         &.selected {
           color: $blue;
         }
+      }
+
+      &-indicator {
+        position: absolute;
+        height: 3px;
+        background: $blue;
+        left: 0;
+        bottom: -1px;
+        width: 100px;
+        transition: all 250ms;
       }
     }
     &-content {
