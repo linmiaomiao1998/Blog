4caa7c3b2440903e6a38bd477cc85ed164fa5592
linmiaomiao1998 <994717247@qq.com>
2020-12-01 20:29:16 +0800

    引入图标

--- neko-ui-vue3/src/components/Topnav.vue
+++ neko-ui-vue3/src/components/Topnav.vue
@@ -1,67 +1,81 @@
 <template>
   <div class="topnav">
-      <div class="logo" >LOGO</div>
-       <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-      <span class="toggleAside" @click="toggleMenu"></span>
+    <div class="logo">
+    <svg class="icon" >
+    <use xlink:href="#icon-jumao"></use>
+</svg></div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
+    <span class="toggleAside" @click="toggleMenu"></span>
   </div>
 </template>
 
 <script lang="ts">
-import {inject, Ref} from 'vue'
-export default {
-    setup(){
-        const asideVisible = inject<Ref<boolean>>('asideVisible')
-        const toggleMenu = ()=>{
-            asideVisible.value=!asideVisible.value;
-        }
-        return{toggleMenu}
-    }
-}
+  import { inject, Ref } from "vue";
+  export default {
+    setup() {
+      const asideVisible = inject<Ref<boolean>>("asideVisible");
+      const toggleMenu = () => {
+        asideVisible.value = !asideVisible.value;
+      };
+      return { toggleMenu };
+    },
+  };
 </script>
 
 <style lang="scss" scope>
-.topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  z-index: 1;
-  justify-content: center;
-  align-items: center;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
+  .topnav {
+  background: rgb(186,215,223);
+background: linear-gradient(180deg, rgba(186,215,223,1) 15%,  rgba(186,215,223,1) 100%);
     display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
+    padding: 16px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 1;
+    justify-content: center;
+    align-items: center;
+    > .logo {
+      max-width: 6em;
+      margin-right: auto;
+      > svg {
+          width: 32px;
+          height: 32px;
+        }
+    }
+    > .menu {
+      display: flex;
+      white-space: nowrap;
+      flex-wrap: nowrap;
+      > li {
+        margin: 0 1em;
+      }
+    }
+    > .toggleAside {
+      display: inline-block;
+      width: 24px;
+      height: 24px;
+      background: transparent;
+      position: absolute;
+      left: 16px;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+    @media (max-width: 500px) {
+      > .menu {
+        display: none;
+      }
+      > .logo {
+        margin: 0 auto;
+      }
+      > .toggleAside {
+        display: inline-block;
+        background: red;
+      }
+     
     }
   }
-  > .toggleAside {
-    display: inline-block;
-    width: 24px;
-    height: 24px;
-    background: transparent;
-    position: absolute;
-    left: 16px;
-    top: 50%;
-    transform: translateY(-50%);
-  }
-  @media (max-width: 500px){
-      >.menu{display: none;}
-      >.logo{margin: 0 auto;}
-      >.toggleAside{display: inline-block;
-      background:red;}
-  }
-}
-
 </style>
--- neko-ui-vue3/src/views/Home.vue
+++ neko-ui-vue3/src/views/Home.vue
@@ -1,43 +1,76 @@
 <template>
-  <Topnav/>
-    <div class="banner">
-      <h1>neko UI</h1>
-      <h2>一个随意的UI框架</h2>
-      <p class="action">
-      <a href=" ">Github</a>
-      <router-link to="/doc">开始</router-link>
-      </p>
+  <div>
+    <div class="topnavandbanner">
+      <Topnav />
+      <div class="banner">
+        <h1>neko UI</h1>
+        <h2>一个随意的UI框架</h2>
+        <p class="action">
+          <a href=" ">Github</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
     </div>
+  </div>
+  <div class="features">
+  <svg class="icon">
+    <use xlink:href="#icon-vue"></use>
+  </svg>
+    <svg class="icon">
+    <use xlink:href="#icon-ts"></use>
+  </svg>
+    <svg class="icon">
+    <use xlink:href="#icon-dengpao"></use>
+  </svg>
+
+  </div>
+  
 </template>
 
 <script lang="ts">
-import Topnav from "../components/Topnav.vue"
+  import Topnav from "../components/Topnav.vue";
 
   export default {
-    components:{Topnav}
+    components: { Topnav },
   };
 </script>
 
 <style lang="scss" scoped>
-.banner {
-  padding: 100px 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-
-  > .actions{
-    padding: 8px 0;
-  }
-   a{
-      margin:0 8px;
-      background: #fff;
-      display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+  $blue: #fcbeaf;
+  $border-radius: 12px;
+  .features{
+    > svg{
+      width: 64px;
+      height: 64px;
     }
+  }
+  .banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    > .actions {
+      padding: 8px 0;
+    }
+    a {
+      margin: 0 8px;
+      background: $blue;
+      display: inline-block;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+    }
+    &:hover {
+      text-decoration: none;
+    }
+  }
+  .topnavandbanner {
+    background: rgb(186, 215, 223);
+    background: linear-gradient(
+      180deg,
+      rgba(186, 215, 223, 1) 15%,
+      rgba(255, 226, 226, 1) 100%
+    );
   }
 </style>
