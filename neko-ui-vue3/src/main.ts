e8178cce3fcba85a8509da75e16c66ba78a2c296
linmiaomiao1998 <994717247@qq.com>
2020-12-05 20:51:48 +0800

    修改路径

--- neko-ui-vue3/src/main.ts
+++ neko-ui-vue3/src/main.ts
@@ -2,7 +2,7 @@ import "./lib/neko.scss";
 import "./index.scss";
 import { createApp } from 'vue'
 import App from './App.vue'
-import { router } from './components/router';
+import { router } from './router';
 import 'github-markdown-css'
 import Markdown from './components/Markdown.vue';
 
--- neko-ui-vue3/src/router.ts
+++ neko-ui-vue3/src/router.ts
@@ -1,18 +1,16 @@
-import { createWebHashHistory, createRouter } from 'vue-router';
-import Home from './views/Home.vue'
-import Doc from './views/Doc.vue'
-import SwitchDemo from './components/SwitchDemo.vue'
-import ButtonDemo from './components/ButtonDemo.vue'
-import DialogDemo from './components/DialogDemo.vue'
-import TabsDemo from './components/TabsDemo.vue'
-import Intro from './views/Intro.vue';
-import GetStarted from './views/GetStarted.vue'
-import Install from './views/Install.vue'
+import { createWebHashHistory, createRouter } from "vue-router";
+import Home from "./views/Home.vue";
+import Doc from "./views/Doc.vue";
+import SwitchDemo from "./components/SwitchDemo.vue";
+import ButtonDemo from "./components/ButtonDemo.vue";
+import DialogDemo from "./components/DialogDemo.vue";
+import TabsDemo from "./components/TabsDemo.vue";
+import { h } from 'vue';
+import Markdown from './components/Markdown.vue';
 import intro from './markdown/intro.md'
 import getStarted from './markdown/get-started.md'
 import install from './markdown/install.md'
-import Markdown from './components/Markdown.vue';
-import { h } from 'vue';
+
 
 const history = createWebHashHistory()
 const md = string => h(Markdown, { content: string, key: string })
@@ -25,9 +23,6 @@ export const router = createRouter(
                 path: "/doc", component: Doc,
                 children: [
                     { path: "", redirect: '/doc/intro' },
-                    { path: "intro", component: Intro },
-                    { path: "get-started", component: GetStarted },
-                    { path: "install", component: Install },
                     { path: "switch", component: SwitchDemo },
                     { path: "button", component: ButtonDemo },
                     { path: "dialog", component: DialogDemo },
